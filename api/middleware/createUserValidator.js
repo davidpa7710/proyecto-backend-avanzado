30ec3a6dbe6e7d4a9581e84606868224b84d8389
David Palacios <davidpa7710@gmail.com>
2022-07-27 16:05:15 -0500

    ✨  feat: validator from user and auth created

---
+++ api/middleware/createUserValidator.js
@@ -0,0 +1,22 @@
+import joi from 'joi'
+
+const userSchema = joi.object({
+    name: joi.string().required(),
+    lastName: joi.string().required(),
+    birthday: joi.date().required(),
+    email: joi.string().required(),
+    phone: joi.string().required(),
+    password: joi.string().required(),
+    role: joi.string().valid(['user', 'admin']).default('user')
+});
+
+export default async (req, res, next) => {
+    try {
+        await userSchema.validateAsync(req.body)
+        next()
+    } catch (error) {
+       return res.status(401).json({ 
+         msg: 'error al un crear user'
+       }) 
+    }
+}
--- api/routes/authRoutes.js
+++ api/routes/authRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express'
 import { authController } from '../controllers/index.js'
+import { createUserValidator } from '../middleware/index.js'
 
 const router = express.Router()
 
-router.post('/auth/register', authController.default.register)
+router.post('/auth/register', createUserValidator, authController.default.register)
 
 router.post('/auth/login', authController.default.login)
 
